2b9c2d3f3e91ffd209ebcfa66aeb6fd3187134a3
詹易衡 <pupu1416@yahoo.com.tw>
2015-08-10 17:07:35 +0800

    init

---
+++ app/assets/javascripts/edit.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require component
+//= require processing
---
+++ app/controllers/processing_controller.rb
@@ -0,0 +1,2 @@
+class ProcessingController < ApplicationController
+end
