d5f7fb3eff0850e45a100e2b5ac4c23eefaca63d
詹易衡 <pupu1416@yahoo.com.tw>
2015-08-14 17:57:27 +0800

    fix

--- app/assets/javascripts/imageEdit.js
+++ app/assets/javascripts/imageEdit.js
@@ -1,46 +1,44 @@
-var margin_width = 22;
-var imgHeight = 302;
-var img_y = 78;
-var totalWidth = 0;
-var current_film_x = 0;
-var current_image_x = 0;
 
-var film_editor;
-var ctx;
-var temp_img;
-var canvas;
 var FilmEditor = function()
 {
-	
-	var filmImg = $('#filmImg').get(0);
+	var margin_width = 22;
+	var imgHeight = 302;
+	var img_y = 78;
+	var totalWidth = 0;
+	var current_film_x = 0;
+	var current_image_x = 0;
+	var canvas_width;
+	var film_editor;
+	var ctx;
+	var temp_img;
+	var canvas;
+	var filmImg;
+
+	canvas = $('#canvas').get(0);
+	filmImg = $('#filmImg').get(0);
+	$(filmImg).load(function()
+		{
+			canvas.width = 460;
+		    canvas.height = 460;
+		    ctx = canvas.getContext('2d');
+		}
+	);
 	
 	//var canvas = document.createElement('canvas');
-	canvas = $('#canvas').get(0);
 	
-    canvas.width = 1000;
-    canvas.height = 460;
-
-    var height = 460;
-
-    ctx = canvas.getContext('2d');
-    for (var i=0 ;i<2;i++)
-    {
-    	addFilmBlock();
-    }
-
     /*
     var img = new Image();
     img.src = 'IMG_0026.jpg';
     resizeImageWithHeight(img,imgHeight);
     addPhoto(img);
 	*/
-
+	/*
 	$('#add-image-button').click(function()
 	{
 		console.log('click');
 		addPhoto(img);
-	});
-
+	});*/
+	
 	function resizeImageWithHeight(img,height)
 	{
 		img.width = img.width/img.height*height;
@@ -48,18 +46,35 @@ var FilmEditor = function()
 	}
 	function addPhoto(image)
 	{
-		while(current_image_x+image.width>current_film_x)
-		{
-			addFilmBlock();
-		}
-		addImage(image);
+		var dis = current_image_x+image.width - current_film_x;
+		var num = dis/filmImg.width;
+		
+		var old  = new Image();
+		old.src = canvas.toDataURL('image/png');
+		
+		$(old).load(function()
+			{
+				canvas.width += filmImg.width * num;
+				ctx.drawImage(old,0,0,old.width,old.height);
+				
+				for (var i=0;i<num;i++)
+				{
+					addFilmBlock();
+
+				}
+				addImage(image);
+			}
+		);
+
+		
 
 	}
+
 	function addFilmBlock()
 	{
-		//add film background
-		ctx.drawImage(filmImg,current_film_x,0,filmImg.width, filmImg.height);
-		current_film_x+=filmImg.width;
+			console.log(canvas.width);
+			ctx.drawImage(filmImg,current_film_x,0,filmImg.width, filmImg.height);
+			current_film_x+=filmImg.width;	
 	}
 	function addImage(image)
 	{
@@ -76,18 +91,22 @@ var FilmEditor = function()
 		ctx.drawImage(filmImg,current_film_x,0,filmImg.width, filmImg.height);
 
 	}
-	function toDataURL()
+	function getCanvasImage()
 	{
-
+		
+		
 	}
 
 	this.readURL = function(input) {
       if (input.files && input.files[0]) {
+      	//create reader
           var reader = new FileReader();
-          console.log($);
+
           function readFile(index)
           {
+
           	if( index >= input.files.length ) return;
+
            	reader.onload = function (e) {
           	  temp_img =new Image();
               temp_img.src = e.target.result;
@@ -117,10 +136,12 @@ function loadImg()
 	film_editor.readURL($('#imgFile').get(0));
 	return false;
 }
-
-$(document).on("page:change", function() {
+var init = function()
+{
 	film_editor = new FilmEditor();	
-});
+}
 
+$(document).ready(init);
+$(window).bind('page:change', init);
 
 
