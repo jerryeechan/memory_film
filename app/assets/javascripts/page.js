09fdea377d9b200f9269d6f0306e6e5e70965c08
jerrypupu111 <pupu1416@yahoo.com.tw>
2015-10-11 02:31:39 +0800

    main page

---
+++ app/assets/javascripts/page.js
@@ -0,0 +1,8 @@
+//= require page/jquery-1.11.2.min
+//= require page/retina 
+//= require page/jquery.fancybox.pack
+//= require page/scripts
+//= require page/jquery.flexslider-min
+//= require page/bootstrap.min
+//= require page/classie
+//= require page/jquery.waypoints.min
